13c75cdad25d01e53627c404c9ca413377e4b833
Harshit0000Sharma <91077653+Harshit0000Sharma@users.noreply.github.com>
2022-08-01 09:00:50 +0530

    Add files via upload

---
+++ Javascript/Chapter 3/10_while_loops.js
@@ -0,0 +1,17 @@
+// while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+
+// do while loop
+n = Number.parseInt(n)
+
+do{
+  console.log(i)
+  i++;
+}while (i < n) 
