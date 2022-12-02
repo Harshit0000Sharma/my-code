3d4ebe1ac22629bff4ed1cabb2c0b81ed29d6651
Harshit Sharma <91077653+Harshit0000Sharma@users.noreply.github.com>
2022-12-02 13:14:39 +0530

    Add files via upload

---
+++ Javascript/Chapter 8/39_classname_classlist.js
@@ -0,0 +1,6 @@
+first.className = "color";
+console.log(first.classList);
+first.classList.remove("color");
+first.classList.add("yellow");
+console.log(first.classList.contains("color"))
+first.classList.toggle("color");
